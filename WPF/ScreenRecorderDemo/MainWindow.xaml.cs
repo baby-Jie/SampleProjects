716f44dd0c8ea670bb66bf60ca0a35b17cccbffb
smxJ0 <995047027@qq.com>
2019-07-10 11:50:00 +0800

    add project ScreenRecorderDemo

---
+++ WPF/ScreenRecorderDemo/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenRecorderDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitialBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ScnLibHelper.CommonInitialRecord();
+        }
+
+        private void StartRecordBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ScnLibHelper.StartRecord();
+        }
+
+        private void PasueRecordBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ScnLibHelper.PauseRecord();
+        }
+
+        private void StopRecordBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ScnLibHelper.StopRecord();
+        }
+
+        private void RecordAudioBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            ScnLibHelper.RecordAudio();
+        }
+    }
+}
---
+++ WPF/ScreenRecorderDemo/ScnLibHelper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenRecorderDemo
+{
+    public class ScnLibHelper
+    {
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <returns></returns>
+        public static bool InitialRecord()
+        {
+            return ZDSoft.SDK.ScnLib_Initialize();
+        }
+
+        public static void CommonInitialRecord()
+        {
+            InitialRecord();
+            EnableAudio(true);
+            EnableAudio(false);
+        }
+
+        /// <summary>
+        /// 开始录制
+        /// </summary>
+        /// <returns></returns>
+        public static bool StartRecord()
+        {
+            return ZDSoft.SDK.ScnLib_StartRecording();
+        }
+
+        /// <summary>
+        /// 暂停录制
+        /// </summary>
+        /// <returns></returns>
+        public static bool PauseRecord()
+        {
+            return ZDSoft.SDK.ScnLib_PauseRecording();
+        }
+
+        /// <summary>
+        /// 停止录制
+        /// </summary>
+        public static void StopRecord()
+        {
+             ZDSoft.SDK.ScnLib_StopRecording();
+        }
+
+        /// <summary>
+        /// 开启音频录制
+        /// </summary>
+        /// <param name="playback"></param>
+        public static void EnableAudio(bool playback)
+        {
+            ZDSoft.SDK.ScnLib_RecordAudioSource(playback, true);
+        }
+
+        /// <summary>
+        /// 关闭音频录制
+        /// </summary>
+        /// <param name="playback"></param>
+        public static void DisableAudio(bool playback)
+        {
+            ZDSoft.SDK.ScnLib_RecordAudioSource(playback, false);
+        }
+
+        /// <summary>
+        /// 录制音频
+        /// </summary>
+        public static void RecordAudio()
+        {
+            // 先清空 video path
+            ZDSoft.SDK.ScnLib_SetVideoPathW(string.Empty);
+
+            string audioPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+            string audioFileName = Path.Combine(audioPath, "test.mp3");
+            ZDSoft.SDK.ScnLib_SetAudioPathW(audioFileName);
+        }
+
+        public static string GetAudioPath()
+        {
+            StringBuilder sb = new StringBuilder(1025);
+            ZDSoft.SDK.ScnLib_GetAudioPathW(sb, true);
+            return sb.ToString();
+        }
+    }
+}
